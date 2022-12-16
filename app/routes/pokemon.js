fd4e1239f124413d697dbcae2b1abc60e9d91c9a
Oriol Sastre <oriol.sastre@gmail.com>
2022-12-16 20:53:38 +0100

    fetch -> cross-fetch

--- app/routes/pokemon.js
+++ app/routes/pokemon.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const fetch = require('cross-fetch');
 
 const router = express.Router();
 
@@ -8,11 +8,14 @@ router.get('/', (req,res) => {
 })
 
 //https://github.com/everydeveloper/node-express-course/blob/master/responses/04-get-data-with-var.md
-router.get('/:id', (req,res) => {
+router.get('/:id', async (req,res) => {
     const pokeID = req.params.id;
-    var pokeAPIuri = `https://pokeapi.co/api/v2/${pokeID}`
-    
-    //res.send("Buscaré el Pokemon amb ID "+pokeID)
+    var pokeAPIuri = `https://pokeapi.co/api/v2/pokemon/${pokeID}`;
+
+    const resposta = await fetch(pokeAPIuri);
+    const dadesRawPoke = await resposta.json();
+
+    res.json(dadesRawPoke)
 })
 
 module.exports = router;
