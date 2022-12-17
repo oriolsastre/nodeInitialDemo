08c1083f673ebe91467f3310a7cf08a4ecfcefc4
Oriol Sastre <oriol.sastre@gmail.com>
2022-12-17 11:30:02 +0100

    express-fileupload

--- app/routes/upload.js
+++ app/routes/upload.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const {siImatgeMW} = require('../middlewares/siImatge');
 
 const router = express.Router();
 
+router.use(fileUpload());
 
 router.get('/', (req,res) => {
     res.render('index', {title: "Pujar imatge"})
 })
 
-router.post('/', (req,res) => {
+router.post('/', siImatgeMW, (req,res) => {
     //req.is(type)
         //https://pqina.nl/blog/upload-image-with-nodejs/
-    res.render("index", {title: "Imatge pujada"})
+    console.log(req.files)
+    const fitxerPujat = req.files.fitxer;
+
+    res.render("index", {title: "Nivell 1, Exercici 2"})
 })
 
 module.exports = router;
