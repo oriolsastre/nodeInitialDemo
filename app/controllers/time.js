3ae1493d5ccba476c9bb12c729234a1f00e7fb7f
Oriol Sastre <oriol.sastre@gmail.com>
2022-12-27 11:40:33 +0100

    gestionar basic auth

--- app/controllers/time.js
+++ app/controllers/time.js
@@ -1,9 +1,18 @@
+const { b64toasc, userPswd } = require('../helpers/basicAut')
+
 const getTime = (req,res) => {
     res.json({Missatge: "Enunciat"})
 }
-
+//Explicar que l'usuari no pot tenir :. Això s'impediria en el formulari d'inscripció.
 const postTime = (req,res) => {
-    res.json({Missatge: "Enunciat"})
+    const [usuari, contrassenya] = userPswd(b64toasc(req.headers.authorization));
+    res.status(200).json({
+        current_datettime: new Date().toLocaleString(),
+        Credencials: {
+            Usuari: usuari,
+            Contrassenya: contrassenya
+        }
+    })
 }
 
 module.exports = { getTime, postTime }
--- app/middlewares/time.js
+++ app/middlewares/time.js
@@ -1,11 +1,25 @@
+const { b64toasc, userPswd } = require('../helpers/basicAut')
+
 const cacheControl = (req, res, next) => {
     res.set('Cache-control', 'no-cache');
     next();
 }
 
 const autoritzacio = (req, res, next) => {
+    const noAutoritzat = motiu => res.status(401).json({Error: motiu});
+    if(!req.headers.authorization){noAutoritzat("No s'han rebut credencials")}
+    else{
+        const credencials = b64toasc(req.headers.authorization)
+        const [usuari, pswd] = userPswd(credencials)
+        if(credencials == ":"){noAutoritzat("No s'han rebut credencials")}
+        else if(usuari.length === 0 || pswd.length === 0){noAutoritzat("Falta l'usuari o la contrassenya")}
+        else{next();}
+    }
+}
+
+const bodyUser = (req, res, next) => {
     next();
 }
 
 
-module.exports = { cacheControl, autoritzacio }
+module.exports = { cacheControl, autoritzacio, bodyUser }
