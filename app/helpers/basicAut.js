aee345dce6347db54c2f67ca8a4efce314105de5
Oriol Sastre <oriol.sastre@gmail.com>
2022-12-27 12:54:54 +0100

    requereix body.user

--- app/helpers/basicAut.js
+++ app/helpers/basicAut.js
@@ -3,7 +3,7 @@ function b64toasc (autoritzacio) {
 }
 
 function userPswd (credencials){
-    //L'usuari no pot contenir ":" per conidició meva, però el password sí que en podria tenir.
+    //L'usuari no pot contenir ":" per norma imposada, però el password sí que en podria tenir.
     //Per això enlloc de split(":"), slice amb la primera ocorrencia de ":"
     const i = credencials.indexOf(":");
     return [credencials.slice(0,i), credencials.slice(i+1)]
