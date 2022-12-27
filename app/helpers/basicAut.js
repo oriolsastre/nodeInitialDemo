18ac05957927fe089fa8c69fa5b42e1e775612c2
Oriol Sastre <oriol.sastre@gmail.com>
2022-12-27 11:40:10 +0100

    canvi de nom helper

---
+++ app/helpers/basicAut.js
@@ -0,0 +1,12 @@
+function b64toasc (autoritzacio) {
+    return credencials = Buffer.from(autoritzacio.split(" ")[1], 'base64').toString()
+}
+
+function userPswd (credencials){
+    //L'usuari no pot contenir ":" per conidició meva, però el password sí que en podria tenir.
+    //Per això enlloc de split(":"), slice amb la primera ocorrencia de ":"
+    const i = credencials.indexOf(":");
+    return [credencials.slice(0,i), credencials.slice(i+1)]
+}
+
+module.exports = { b64toasc, userPswd }
