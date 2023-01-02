9ee9d15b5bbac5243b3f7a7bc5936c3cbcdfa127
Oriol Sastre <oriol.sastre@gmail.com>
2023-01-02 14:13:13 +0100

    MW siImatge

--- app/middlewares/siImatge.js
+++ app/middlewares/siImatge.js
@@ -3,14 +3,15 @@
  */
 const siImatge = (req,res,next) => {
     if(!req.files || !req.files.fitxer){res.status(418).send({Error: "No s'ha tramitat cap fitxer."})}
-    
-    const format_extensio = req.files.fitxer.mimetype.split("/");
-    if(format_extensio[0] == "image"){
-        const extensioValida = ['jpg', 'jpeg', 'png', 'gif']
-        if(extensioValida.includes(format_extensio[1])){
-            next();
-        }else{res.status(415).send({Error: "Format d'imatge no acceptat. Ha de ser jpg/jpeg, png o gif."})}
-    }else{res.status(415).send({Error: "Document no acceptat. No és una imatge."})}
+    else{
+        const format_extensio = req.files.fitxer.mimetype.split("/");
+        if(format_extensio[0] == "image"){
+            const extensioValida = ['jpg', 'jpeg', 'png', 'gif']
+            if(extensioValida.includes(format_extensio[1])){
+                next();
+            }else{res.status(415).send({Error: "Format d'imatge no acceptat. Ha de ser jpg/jpeg, png o gif."})}
+        }else{res.status(415).send({Error: "Document no acceptat. No és una imatge."})}
+    }
 }
 
 exports.siImatgeMW = siImatge;
