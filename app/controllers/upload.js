3e2a25158962f06302062aaca242a3d40791d84b
Oriol Sastre <oriol.sastre@gmail.com>
2023-01-08 12:54:27 +0100

    nom de pujada amb timestamp

--- app/controllers/upload.js
+++ app/controllers/upload.js
@@ -1,16 +1,20 @@
-const getUpload = (req,res) => {
-    res.render('index', {title: "Pujar imatge"})
-}
-
 const postUpload = (req,res) => {
     const imatge = req.files.fitxer;
-    const rutaPujada = './upload/fitxer.'+imatge.mimetype.split("/")[1];
+    const nameTimeStamp = new Date().toISOString().replace(/\.|:/g,'');
+    const rutaPujada = './upload/'+nameTimeStamp+'.'+imatge.mimetype.split("/")[1];
 
     //comentar imatge.mv() si no es vol desar el fitxer al servidor.
     imatge.mv(rutaPujada, err => {
         if(err){return res.status(500).send(err)}
+        else{
+            res.status(200).send({
+                Nom: imatge.name,
+                Mida: imatge.size,
+                Tipus: imatge.mimetype,
+                Ruta: rutaPujada,
+                Status: "Imatge pujada correctament al servidor"})
+        }
     })
-    res.status(200).send({Nom: imatge.name, Mida: imatge.size, Tipus: imatge.mimetype, Status: "Imatge pujada correctament al servidor"})
 }
 
-module.exports = { getUpload, postUpload }
+module.exports = { postUpload }
--- app/routes/upload.js
+++ app/routes/upload.js
@@ -2,12 +2,11 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const router = express.Router();
 
-const { getUpload, postUpload } = require('../controllers/upload')
+const { postUpload } = require('../controllers/upload')
 const { siImatgeMW } = require('../middlewares/siImatge');
 
 router.use(fileUpload({createParentPath:true}));
 
-router.get('/', getUpload)
 router.post('/', siImatgeMW, postUpload)
 
 module.exports = router;
