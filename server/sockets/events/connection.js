ebcedd1cace99a3caae37991812df1f84bf11a5c
Oriol Sastre <oriol.sastre@gmail.com>
2023-02-21 11:17:15 +0100

    css

--- server/sockets/events/connection.js
+++ server/sockets/events/connection.js
@@ -4,14 +4,14 @@ const { fetchRooms } = require('../../helpers/room');
 const connection = async (io, socket, data) => {
     /* Avises a tohom que t'has connectat al xat  */
     socket.broadcast.emit('user-connected', data.usuari)
-    
+
     /* En connectar-te carregues les sales disponibles al xat i els usuaris connectats */
     let chatRooms = await fetchRooms();
     chatRooms.shift();  //main ja la tenim.
-    socket.emit('user-loadFirst', {chatRooms, connectedUsers: data.connectedUsers});
+    socket.emit('user-loadFirst', { chatRooms, connectedUsers: data.connectedUsers });
 
 
-    
+
     /* Per defecte, en connectar-te al xat entres a la sala principal: 1 */
     await socket.join('1')
     //socket.to('1').emit('user-joinedRoom', data.usuari.name)
